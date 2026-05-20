aea89f9041b41f5600e588ec0dca79216671e3be
Ayrton Ferguson <ayrton.ferguson@outlook.com>
2026-05-21 10:22:14 +1200

    Cleaned code into different scripts. Main script barely has any code but runs.

--- StartBattle.py
+++ StartBattle.py
@@ -1,24 +1,29 @@
 import enemyBattle
 
-enemyTypeWorld1 = ["goblin", "boulderman", "slime"]
-enemyStatHealthWorld1 = [5, 15, 10]
-enemyStatBDamageWorld1 = [3, 1, 2]
-enemyStatMinDamageWorld1 = [5, 1, 3]
-enemyStatMaxDamgeWorld1 = [15, 10, 4]
-maxPlayerHealth = 15
+class battle:
+    enemyTypeWorld1 = ["goblin", "boulderman", "slime"]
+    enemyStatHealthWorld1 = [5, 15, 10]
+    enemyStatBDamageWorld1 = [3, 1, 2]
+    enemyStatMinDamageWorld1 = [5, 1, 3]
+    enemyStatMaxDamgeWorld1 = [15, 10, 4]
+    maxPlayerHealth = 15
+    
+    def selectEnemyWorld (world):
+        if world == 1:
+            enemyBattle.fightEnemy.whatEnemy(
+                battle.enemyTypeWorld1,
+                battle.enemyStatHealthWorld1,
+                battle.enemyStatBDamageWorld1,
+                battle.enemyStatMinDamageWorld1,
+                battle.enemyStatMaxDamgeWorld1,
+                battle.maxPlayerHealth,
+            )
 
-enemyBattle.fightEnemy.whatEnemy(enemyTypeWorld1,
-    enemyStatHealthWorld1,
-    enemyStatBDamageWorld1,
-    enemyStatMinDamageWorld1,
-    enemyStatMaxDamgeWorld1,
-    maxPlayerHealth,
-)
-
-print(enemyBattle.playerHealth)
-enemyBattle.fightEnemy.beginBattle(enemyBattle.enemy)
-while enemyBattle.enemyAlive and enemyBattle.playerHealth > 0:
-    enemyBattle.fightEnemy.theirTurn(enemyBattle.enemy)
-    if not enemyBattle.enemyAlive or enemyBattle.playerHealth <= 0:
-        break
-    enemyBattle.fightEnemy.yourTurn(enemyBattle.enemy, 2, 1, 5)
+    def runBattle ():
+        print(enemyBattle.playerHealth)
+        enemyBattle.fightEnemy.beginBattle(enemyBattle.enemy)
+        while enemyBattle.enemyAlive and enemyBattle.playerHealth > 0:
+            enemyBattle.fightEnemy.theirTurn(enemyBattle.enemy)
+            if not enemyBattle.enemyAlive or enemyBattle.playerHealth <= 0:
+                break
+            enemyBattle.fightEnemy.yourTurn(enemyBattle.enemy, 2, 1, 5)
